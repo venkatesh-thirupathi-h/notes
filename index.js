f226b23903c9a87e17722363175af316f3b9ab87
venkatesh <shariharanvenkatesh@gmail.com>
2022-08-21 17:23:01 +0530

    added react build

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.static("build"));
+app.use(express.json());
+app.use(cors());
 
 
 let notes = [
